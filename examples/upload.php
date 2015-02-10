7c42f01f3a1b82ae03ad7ff143ea16a55453456a
ridesoft <maurizio.brioschi@ridesoft.org>
2015-02-10 14:58:14 +0100

    fix on update example

--- examples/upload.php
+++ examples/upload.php
@@ -6,5 +6,5 @@ use ridesoft\Azure\AzureIO;
 $config = require_once 'src/config/config.php';
 $azure = new AzureIO($config['azure']['connectionstring']);
 
-$azure->copy('pdf', 'e3e50cf02910fe819538030ce2dd498f/vol_07_sat.pdf', '/var/www/html/static.paperapp.local/e3e50cf02910fe819538030ce2dd498f/tmp/vol_07_sat.pdf');
-echo var_dump($azure->scandir('pdf/e3e50cf02910fe819538030ce2dd498f'));
+$azure->copy('pdf', 'e3e50cf02910fe819538030ce2dd498f/test.pdf', '/var/www/html/test/vol_07_sat.pdf');
+echo var_dump($azure->scandir('pdf'));
