4359aacd371bd416c9169782afb91d68001f0761
ridesoft <maurizio.brioschi@ridesoft.org>
2015-02-12 15:21:57 +0100

    add mkdir method for create container

--- src/ridesoft/AzureCloudMap/AzureIO.php
+++ src/ridesoft/AzureCloudMap/AzureIO.php
@@ -25,6 +25,12 @@ class AzureIO extends AzureMapping {
             file_put_contents($destinationFilename, stream_get_contents($blob->getContentStream()));
             return true;
         } catch (ServiceException $e) {
+            // Handle exception based on error codes and messages.
+            // Error codes and messages are here: 
+            // http://msdn.microsoft.com/it-it/library/windowsazure/dd179439.aspx
+            $code = $e->getCode();
+            $error_message = $e->getMessage();
+            echo $code . ": " . $error_message . "<br />";
             return false;
         }
     }
@@ -44,6 +50,12 @@ class AzureIO extends AzureMapping {
             }
             return $objects;
         } catch (Exception $ex) {
+            // Handle exception based on error codes and messages.
+            // Error codes and messages are here: 
+            // http://msdn.microsoft.com/it-it/library/windowsazure/dd179439.aspx
+            $code = $e->getCode();
+            $error_message = $e->getMessage();
+            echo $code . ": " . $error_message . "<br />";
             return null;
         }
     }
@@ -58,6 +70,12 @@ class AzureIO extends AzureMapping {
         try {
             $this->blobRestProxy->deleteBlob($dir, $file);
         } catch (ServiceException $e) {
+            // Handle exception based on error codes and messages.
+            // Error codes and messages are here: 
+            // http://msdn.microsoft.com/it-it/library/windowsazure/dd179439.aspx
+            $code = $e->getCode();
+            $error_message = $e->getMessage();
+            echo $code . ": " . $error_message . "<br />";
             return false;
         }
     }
@@ -73,6 +91,12 @@ class AzureIO extends AzureMapping {
             $this->blobRestProxy->deleteContainer($dir);
             return true;
         } catch (ServiceException $e) {
+            // Handle exception based on error codes and messages.
+            // Error codes and messages are here: 
+            // http://msdn.microsoft.com/it-it/library/windowsazure/dd179439.aspx
+            $code = $e->getCode();
+            $error_message = $e->getMessage();
+            echo $code . ": " . $error_message . "<br />";
             return false;
         }
     }
@@ -91,6 +115,61 @@ class AzureIO extends AzureMapping {
             $this->blobRestProxy->createBlockBlob($dest_dir, $dest_blob, $content);
             return true;
         } catch (ServiceException $e) {
+            // Handle exception based on error codes and messages.
+            // Error codes and messages are here: 
+            // http://msdn.microsoft.com/it-it/library/windowsazure/dd179439.aspx
+            $code = $e->getCode();
+            $error_message = $e->getMessage();
+            echo $code . ": " . $error_message . "<br />";
+            return false;
+        }
+    }
+    /**
+     * create a container
+     * @param type $dir name of container
+     * @param type $access can be cb(CONTAINER_AND_BLOBS) or b (BLOBS_ONLY)
+     * CONTAINER_AND_BLOBS:     
+     * Specifies full public read access for container and blob data.
+     * proxys can enumerate blobs within the container via anonymous 
+     * request, but cannot enumerate containers within the storage account.
+     *
+     * BLOBS_ONLY:
+     * Specifies public read access for blobs. Blob data within this 
+     * container can be read via anonymous request, but container data is not 
+     * available. proxys cannot enumerate blobs within the container via 
+     * anonymous request.
+     * If this value is not specified in the request, container data is 
+     * private to the account owner.
+     * @return boolean
+     */
+    public function mkdir($dir,$access='cb', array $metadata=array()) {
+        $createContainerOptions = new CreateContainerOptions();
+        switch ($access)    {
+            case 'cb':
+                $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
+                break;
+            case 'b':
+                $createContainerOptions->setPublicAccess(PublicAccessType::BLOBS_ONLY);
+                break;
+            default:
+                $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);            
+        }
+        
+        foreach($metadata as $key=>$value) {
+            $createContainerOptions->addMetaData($key, $value);
+        }
+        
+        try {
+            // Create container.
+            $this->blobRestProxy->createContainer($dir, $createContainerOptions);
+            return true;
+        } catch (ServiceException $e) {
+            // Handle exception based on error codes and messages.
+            // Error codes and messages are here: 
+            // http://msdn.microsoft.com/it-it/library/windowsazure/dd179439.aspx
+            $code = $e->getCode();
+            $error_message = $e->getMessage();
+            echo $code . ": " . $error_message . "<br />";
             return false;
         }
     }
