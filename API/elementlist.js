dc7356e830f25a86a98344ef6cff1d5f8bcb32f0
ridesoft <maurizio.brioschi@ridesoft.org>
2015-02-12 15:24:40 +0100

    API

---
+++ API/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","ridesoft\\AzureCloudMap\\AzureCloudMapServiceProvider"],["c","ridesoft\\AzureCloudMap\\AzureIO"],["c","ridesoft\\AzureCloudMap\\AzureMapping"],["c","ridesoft\\AzureCloudMap\\Facades\\AzureIO"]];
