7a1e9809df73ffa8607df27abc02f3239bc0b4f1
ridesoft <maurizio.brioschi@ridesoft.org>
2015-02-12 16:54:06 +0100

    tests

--- tests/AzureIoTest.php
+++ tests/AzureIoTest.php
@@ -7,57 +7,60 @@ use ridesoft\AzureCloudMap\AzureIO;
  * @author maurizio brioschi <maurizio.brioschi@ridesoft.org>
  */
 class AzureIoTest extends PHPUnit_Framework_TestCase{
-    protected $azure;
     protected $config;
     
     protected function setUp() {
-        parent::setUp();
-        $this->config = require_once 'src/config/config.php';
-       
+        $this->config = require 'src/config/config.php';     
     }
     /**
-     * @dataProvider downloadCorrectProvider
+     * @dataProvider mkdirProvider
      */
-    public function testDownload($container,$file,$destination)  {
-        $this->azure = new AzureIO($this->config);
-        $this->assertTrue($this->azure->download($container,$file,$destination));
-        unlink($destination);
-    }
-    
-    public function testScandir()   {
-         $this->azure = new AzureIO($this->config);
-        $objects = $this->azure->scandir('pdf');
-        $this->assertArrayHasKey('shit',$objects);
-    }
-    
-    public function testCopy()  {
-        $this->azure = new AzureIO($this->config);
-        $this->azure->copy('pdf','rock/test.php','/home/maurizio/Desktop/test.pdf');
+    public function testMkdir($dir,$access,$metadata) {
+        $azure = new AzureIO($this->config);
+        $this->assertTrue($azure->mkdir($dir,$access,$metadata));
     }
     /**
-     * @dataProvider downloadWrongProvider
+     * @depends testMkdir
      */
-    public function testFalseDownload($container,$file,$destination)  {
-        $this->assertFalse($this->azure->download($container,$file,$destination));
-        
-    }
-   
-    
-    
-    
-    public function downloadCorrectProvider(){
-        return [
-            [
-                'pdf','ciao.pdf','rock.pdf'
-            ]
-        ];
+    public function testCopyandScanDirDownload()  {
+        $azure = new AzureIO($this->config);
+        $this->assertTrue($azure->copy('test','skate.txt','tests/test.txt'));
+        $objects = $azure->scandir('test');
+        $this->assertGreaterThan(0, count($objects));
+        $this->assertTrue($azure->download('test','skate.txt','tests/destroy.txt'));
+        $this->assertTrue(file_exists('tests/destroy.txt'));
+        unlink('tests/destroy.txt');
     }
     
-    public function downloadWrongProvider(){
+    /**
+     * @dataProvider mkdirProvider
+     */
+    public function testMkdirFail($dir,$access,$metadata) {
+        $azure = new AzureIO($this->config);
+        $this->assertFalse($azure->mkdir($dir,$access,$metadata));
+    }
+    /**
+     * @depends testCopyandScanDirDownload
+     */
+    public function unlink($dir, $file){
+        $azure = new AzureIO($this->config);
+        $azure->unlink('test', 'skate.txt');
+        $objects = $azure->scandir('test');
+        $this->assertEquals(0, count($objects));
+    }
+    /**
+     * @dataProvider mkdirProvider
+     */
+    public function rmdir($dir){
+        $azure = new AzureIO($this->config);
+        $azure->rmdir($dir);
+    }
+    
+    public function mkdirProvider() {
         return [
-            [
-                'pdf','skate.pdf','isshit.pdf'
-            ]
+              ['test','cb',[]],
+              ['test2','b',[]],
+              ['test3','cb',['author'=>'Mauri']]
         ];
     }
 }
