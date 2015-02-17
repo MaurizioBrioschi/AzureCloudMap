8d8a375c0489a860db4b2f55897cac27b8ebd3a0
ridesoft <maurizio.brioschi@ridesoft.org>
2015-02-17 12:54:28 +0100

    API

--- examples/scandir.php
+++
@@ -1,7 +0,0 @@
-<?php
-require __DIR__.'/../vendor/autoload.php';
-use ridesoft\AzureCloudMap\AzureIO;
-$config = require_once 'src/config/config.php';
-$azure = new AzureIO($config);
-echo var_dump($azure->scandir('pdf'));
-
--- src/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'azure' => [
-        'connectionstring' => 'myconnection string',
-        'base_url' => 'http://test.blob.core.windows.net'
-    ]
-];
-
