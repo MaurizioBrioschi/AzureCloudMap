993e801ae64852e7f9490956ce28680d558a8aa0
ridesoft <maurizio.brioschi@ridesoft.org>
2015-02-18 10:08:47 +0100

    fix

--- examples/copy.php
+++ examples/copy.php
@@ -3,5 +3,5 @@ require __DIR__.'/../vendor/autoload.php';
 use ridesoft\AzureCloudMap\AzureIO;
 $config = require_once 'src/config/config.php';
 $azure = new AzureIO($config);
-$azure->copy('pdf', 'skateboard.pdf', '/var/www/html/snowboard.pdf');
+$azure->copy('pdf', 'skateboard.pdf', '/home/maurizio/Desktop/CVEU_2015.pdf');
 
